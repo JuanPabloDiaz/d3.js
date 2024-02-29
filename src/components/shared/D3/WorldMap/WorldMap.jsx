0a0831b5aa4e7f988a2272eecd61496788a99b68
JuanPabloDiaz <juanchodis@hotmail.com>
2024-02-29 06:56:11 -0500

    Refactor WorldMap.jsx component

--- src/components/shared/D3/WorldMap/WorldMap.jsx
+++ src/components/shared/D3/WorldMap/WorldMap.jsx
@@ -31,33 +31,34 @@
 
 // ****************************************************************************************
 
-import React, { useEffect, useRef } from 'react';
-import * as d3 from 'd3';
-import * as topojson from 'topojson';
+import React, { useEffect, useRef } from "react";
+import * as d3 from "d3";
+import * as topojson from "topojson";
 
 export const WorldMap_D3 = () => {
   const ref = useRef();
 
   useEffect(() => {
-    const svg = d3.select(ref.current).attr('width', 640).attr('height', 400);
+    const svg = d3.select(ref.current).attr("width", 640).attr("height", 450);
 
-    const projection = d3.geoMercator();
+    const projection = d3.geoMercator().scale(100).translate([320, 300]);
     const path = d3.geoPath(projection);
 
     const WorldMap_api =
-      'https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json';
+      "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json";
 
-    const g = svg.append('g');
+    const g = svg.append("g");
+    // const g = svg.append("g").attr("transform", "translate(0,50)"); // move the group up by 50 pixels
 
-    d3.json(WorldMap_api).then(data => {
+    d3.json(WorldMap_api).then((data) => {
       const countries = topojson.feature(data, data.objects.countries);
-      console.log('countries: ', countries);
-      g.selectAll('path')
+      console.log("countries: ", countries);
+      g.selectAll("path")
         .data(countries.features) // changed from countries.feature to countries.features
         .enter()
-        .append('path')
-        .attr('class', 'country')
-        .attr('d', path); // changed from .attr(d, path) to .attr('d', path)
+        .append("path")
+        .attr("class", "country")
+        .attr("d", path); // changed from .attr(d, path) to .attr('d', path)
     });
   }, []);
 
