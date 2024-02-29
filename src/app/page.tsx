bfb376edff77e009bb11a73e8e62d8105d6cac43
JuanPabloDiaz <juanchodis@hotmail.com>
2024-02-29 07:07:01 -0500

    Add SVG rendering for components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,12 @@ export default function Home() {
           This is a simple Next.js app that uses D3.js to render multiple
           components.
         </p>
+        <p className="text-gray-800 italic border p-2 rounded-lg bg-slate-200">
+          I am not using images, but rather
+          <span className="text-green-600 p-1">Scalable Vector Graphics</span>
+          (SVGs) to render the components. It is a powerful way to render
+          graphics by using DATA to create and manipulate SVGs.
+        </p>
         <p className="text-gray-700">
           The app is a work in progress and is intended to be a learning
           experience for me.
@@ -20,19 +26,12 @@ export default function Home() {
           include a world map, a country chart, a word cloud and much more.
         </p>
         <p className="text-gray-700">
-          To learn more about D3.js and my learning process, please visit the
+          To learn more about D3.js, please visit the
           <a
             href="https://d3js.org/"
             className="text-blue-500 hover:underline hover:text-blue-300 p-1"
           >
             D3.js
-          </a>
-          website and the
-          <a
-            href="https://nextjs.org/"
-            className="text-blue-500 hover:underline hover:text-blue-300 p-1"
-          >
-            Next.js
           </a>
           website.
         </p>
